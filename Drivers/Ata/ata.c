82a689881cf05b207fae47027c2aeaac808ba3f7
OpenSoftware-World <143949134+OpenSoftware-World@users.noreply.github.com>
2026-04-11 00:20:19 +0300

    A Ata disk driver has been added for OpenKernel.

---
+++ Drivers/Ata/ata.c
@@ -0,0 +1,54 @@
+#include "ata.h"
+#include "../../SystemLib/SystemIO/io.h"
+
+static void ata_wait_busy() {
+    while (inb(ATA_STATUS) & ATA_STATUS_BSY);
+}
+
+static void ata_wait_drq() {
+    while(!(inb(ATA_STATUS) & ATA_STATUS_DRQ));
+}
+
+void ata_sys_disk_init() {
+
+}
+
+void ata_sys_disk_read(uint32_t lba, uint8_t* buffer) {
+    ata_wait_busy();
+
+    outb(ATA_DRIVE_SELECT, 0x0E | ((lba >> 24) & 0x0F)); // Select drive and set LBA bits 24-27
+    outb(ATA_SECTOR_COUNT, 1); // Read one sector
+
+    outb(ATA_LBA_LOW, (uint8_t)(lba));
+    outb(ATA_LBA_MID, (uint8_t)(lba >> 8));
+    outb(ATA_LBA_HIGH, (uint8_t)(lba >> 16));
+
+    outb(ATA_COMMAND, ATA_READ);
+
+    ata_wait_busy();
+    ata_wait_drq();
+
+    for (int i = 0; i < 256; i++) {
+        ((uint16_t*)buffer)[i] = inw(ATA_DATA);
+    }
+}
+
+void ata_sys_disk_write(uint32_t lba, const uint8_t* buffer) {
+    ata_wait_busy();
+
+    outb(ATA_DRIVE_SELECT, 0x0E | ((lba >> 24) & 0x0F)); // Select drive and set LBA bits 24-27
+    outb(ATA_SECTOR_COUNT, 1); // Write one sector
+
+    outb(ATA_LBA_LOW, (uint8_t)(lba));
+    outb(ATA_LBA_MID, (uint8_t)(lba >> 8));
+    outb(ATA_LBA_HIGH, (uint8_t)(lba >> 16));
+
+    outb(ATA_COMMAND, ATA_WRITE);
+
+    ata_wait_busy();
+    ata_wait_drq();
+
+    for (int i = 0; i < 256; i++) {
+        outw(ATA_DATA, ((uint16_t*)buffer)[i]);
+    }
+}
---
+++ Drivers/Ata/ata.h
@@ -0,0 +1,30 @@
+#ifndef ATA_H
+#define ATA_H
+
+/*
+ * ATA Driver for OpenKernel
+*/
+
+#include "../../SystemLib/Std/types.h"
+
+#define ATA_DATA 0x1F0
+#define ATA_ERROR 0x1F1
+#define ATA_SECTOR_COUNT 0x1F2
+#define ATA_LBA_LOW 0x1F3
+#define ATA_LBA_MID 0x1F4
+#define ATA_LBA_HIGH 0x1F5
+#define ATA_DRIVE_SELECT 0x1F6
+#define ATA_COMMAND 0x1F7
+#define ATA_STATUS 0x1F7
+
+#define ATA_READ 0x20
+#define ATA_WRITE 0x30
+
+#define ATA_STATUS_BSY 0x80
+#define ATA_STATUS_DRQ 0x08
+
+void ata_sys_disk_init();
+void ata_sys_disk_read(uint32_t lba, uint8_t* buffer);
+void ata_sys_disk_write(uint32_t lba, const uint8_t* buffer);
+
+#endif
