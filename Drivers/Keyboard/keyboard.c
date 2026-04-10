a30f7d8144a694a91b00575e803bd56e2938afc3
OpenSoftware-World <143949134+OpenSoftware-World@users.noreply.github.com>
2026-04-10 22:59:19 +0300

    New features have been added to the Keyboard driver.

--- Drivers/Keyboard/keyboard.c
+++ Drivers/Keyboard/keyboard.c
@@ -4,6 +4,8 @@
 #define KBD_DATA 0x60
 #define KBD_STAT 0x64
 
+static bool_t caps_lock = false;
+
 static char kmap[128] = {
     0,  27, '1','2','3','4','5','6','7','8','9','0','-','=', '\b',
     '\t','q','w','e','r','t','y','u','i','o','p','[',']','\n',
@@ -16,9 +18,17 @@ static char kbf[KEY_BF_SIZE];
 static uint8_t buf_head = 0;
 static uint8_t buf_tail = 0;
 
+static char to_upper(char c) {
+    if (caps_lock && c >= 'a' && c <= 'z') {
+        return c - 32;
+    }
+    return c;
+}
+
 void kbd_init() {
     buf_head = 0;
     buf_tail = 0;
+    caps_lock = false;
 }
 
 uint8_t kb_check() {
@@ -27,28 +37,34 @@ uint8_t kb_check() {
 
 char get_char() {
     uint8_t scancode;
-    
+
     while (1) {
         while (!(inb(KBD_STAT) & 1));
+
         scancode = inb(KBD_DATA);
 
-        if (scancode & 0x80)
+        if (scancode & 0x80) {
             continue;
+        }
 
-        if (scancode < 128)
-            return kmap[scancode];
+        kbd_hndlr(scancode);
+
+        if (scancode < 128) {
+            char c = kmap[scancode];
+
+            if (c == 0) {
+                continue;
+            }
+            
+            c = to_upper(c);
+
+            return c;
+        }
     }
 }
 
-void kbd_hndlr() {
-    uint8_t status = inb(KBD_STAT);
-    if (status & 0x01) {
-        uint8_t scancode = inb(KBD_DATA);
-        char c = 0;
-        if (scancode < 128) c = kmap[scancode];
-        if (c) {
-            kbf[buf_head] = c;
-            buf_head = (buf_head + 1) % KEY_BF_SIZE;
-        }
+void kbd_hndlr(uint8_t scancode) {
+    if (scancode == 0x3A) {
+        caps_lock = !caps_lock;
     }
 }
--- Drivers/Keyboard/keyboard.h
+++ Drivers/Keyboard/keyboard.h
@@ -8,10 +8,10 @@
 void kbd_init();
 char get_char();
 uint8_t kb_check();
-void kbd_hndlr();
+void kbd_hndlr(uint8_t scancode);
 
 #define KBD_DRIVER_NAME "OpenKernel Keyboard Driver"
-#define KBD_DRIVER_VER "0.1"
+#define KBD_DRIVER_VER "0.2"
 #define KBD_DRIVER_DESC "A simple Keyboard driver for OpenKernel"
 #define KBD_DRIVER_KRNL_VER "1.0"
 
