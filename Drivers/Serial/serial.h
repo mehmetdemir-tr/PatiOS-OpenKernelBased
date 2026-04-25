1cce055a9ca14e293931ca7c0661aaaeaecdbe28
OpenSoftware-World <143949134+OpenSoftware-World@users.noreply.github.com>
2026-04-25 19:06:45 +0300

    The necessary information for the serial (COM1_PORT) driver has been added.

--- Drivers/Serial/serial.h
+++ Drivers/Serial/serial.h
@@ -11,4 +11,10 @@ int serial_is_transmit_empty();
 void serial_print_char(char c);
 void serial_print_string(const char *string);
 
+#define SERIAL_DRIVER_NAME "OpenKernel Serial (COM1_PORT) Driver"
+#define SERIAL_DRIVER_VER "0.1"
+#define SERIAL_DRIVER_DESC "A simple VGA text mode driver for OpenKernel"
+#define SERIAL_DRIVER_AUTHOR "OpenSoftware-World"
+#define SERIAL_DRIVER_KRNL_VER "2.0"
+
 #endif
